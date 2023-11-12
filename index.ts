a0dcbfb36a0f0a291f6eed363ddb8515e6720299
Henrik Hofmeister <henrik@newdawn.dk>
2023-11-12 19:56:41 +0100

    fix: Build issues

--- index.ts
+++ index.ts
@@ -1,2 +1,4 @@
-export * from './src/lookup'
-export * from './src/launch'
+export * from './src/lookup';
+export * from './src/launch';
+export * from './src/shared';
+export * from './src/found-editor';
